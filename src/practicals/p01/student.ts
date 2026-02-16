4ccc8cacda9caa86e6f98c7540d18ae8799dafa3
Wenus Sakaekhum <68011036@kmitl.ac.th>
2026-02-16 09:55:00 +0700

    555

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,1 +1,13 @@
-export class Student {}
+export class Student {
+     firstname : string ;
+     lastname : string ;
+    constructor(firstname = '', lastname = '') {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    
+    printName() {
+        console.log(`${this.firstname} ${this.lastname}`);
+    }
+}
