74af3ae748278f2c61619b5dc280706571fb4118
minivalley01 <nppot.winas@gmail.com>
2026-02-16 10:15:35 +0700

    Update person.ts

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,18 @@
-export class Person {}
+export class Person {
+    firstname : string ;
+    lastname : string ;
+    constructor (firstname ="" , lastname = "") {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    
+    getFullName () {
+        return [this.firstname, this.lastname].join(' ');
+    }
+    setAge() {
+        private_Age() {
+            
+            
+        }
+    }
+}
